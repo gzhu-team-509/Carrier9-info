82df4263766aa8dc0cd3cb4f93e77dcca52434af
lightyears <lightyears1998@hotmail.com>
2019-09-17 16:45:32 +0800

    退役Googlehosts

--- files/software/scripts/update_host.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget -O /etc/googlehosts https://raw.githubusercontent.com/googlehosts/hosts/master/hosts-files/hosts
-wget -O /etc/googlehosts6 https://raw.githubusercontent.com/googlehosts/hosts-ipv6/master/hosts-files/hosts
